630ac5584ccb4a86edd81c5cf6d91452e33d19a6
Lukas <info@brainlesslukas.com>
2026-07-16 21:35:09 +0200

    feat: devenv scripts

--- devenv.nix
+++ devenv.nix
@@ -4,4 +4,23 @@
    languages.javascript.enable = true;
    languages.javascript.npm.enable = true;
    languages.javascript.pnpm.enable =  true;
+
+  packages = with pkgs; [ 
+    platformio 
+    figlet
+    lolcat
+  ];
+
+  scripts = {
+    "next:dev".exec = "pnpm next dev";
+    "next:build".exec = "pnpm next build";
+    "next:start".exec = "pnpm next start";
+    "next:lint".exec = "pnpm eslint";
+  };
+
+  enterShell = ''
+    echo
+    figlet -f slant "ctrlPad" | lolcat -p 1
+    echo
+  '';
 }
