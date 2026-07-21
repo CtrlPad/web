3d4c232886aef8b3ae2fa1826c06c64786dfdd2e
brainlesslukas <info@brainlesslukas.com>
2026-07-21 16:23:40 +0200

    chore: format coodebase

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import Hero from "./_sections/Hero";
 
 export default function Home() {
-    return (
-        <main>
-            <Hero />
-        </main>
-    );
+  return (
+    <main>
+      <Hero />
+    </main>
+  );
 }
