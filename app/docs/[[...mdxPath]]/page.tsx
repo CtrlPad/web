c2543ac55ae20e0c8200595a084f466913e7322c
brainlesslukas <info@brainlesslukas.com>
2026-07-29 18:19:43 +0200

    fix: types

--- app/docs/[[...mdxPath]]/page.tsx
+++ app/docs/[[...mdxPath]]/page.tsx
@@ -1,17 +1,19 @@
 import { generateStaticParamsFor, importPage } from "nextra/pages";
-import { useMDXComponents as getMDXComponents } from "../../../mdx-components.ts";
+import { useMDXComponents as getMDXComponents } from "@/mdx-components";
 
 export const generateStaticParams = generateStaticParamsFor("mdxPath");
 
-export async function generateMetadata(props) {
+type Props = { params: Promise<{ mdxPath: string[] }> };
+
+export async function generateMetadata(props: Props) {
   const params = await props.params;
   const { metadata } = await importPage(params.mdxPath);
   return metadata;
 }
 
-const Wrapper = getMDXComponents().wrapper;
+const Wrapper = getMDXComponents({}).wrapper;
 
-export default async function Page(props) {
+export default async function Page(props: Props) {
   const params = await props.params;
   const { default: MDXContent, toc, metadata, sourceCode } = await importPage(params.mdxPath);
   return (
