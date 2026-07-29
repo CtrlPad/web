59c3b42ab8d4455a06436e635865cab08dee9ed7
brainlesslukas <info@brainlesslukas.com>
2026-07-29 18:08:54 +0200

    fix: chold type

--- app/docs/[[...mdxPath]]/layout.tsx
+++ app/docs/[[...mdxPath]]/layout.tsx
@@ -2,7 +2,11 @@ import { Layout } from "nextra-theme-docs";
 import { getPageMap } from "nextra/page-map";
 import "nextra-theme-docs/style.css";
 
-export default async function NextraLayout({ children }) {
+export default async function NextraLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <Layout pageMap={await getPageMap()} docsRepositoryBase="https://github.com/ctrlpad/web/tree/main/docs">
       {children}
