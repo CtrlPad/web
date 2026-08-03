948f0ddc846afa33ffc520d5d81163276e1510f8
brainlesslukas <info@brainlesslukas.com>
2026-08-03 16:03:45 +0200

    chore: lint

--- types/esp-web-tools.d.ts
+++ types/esp-web-tools.d.ts
@@ -3,10 +3,7 @@ import type { DetailedHTMLProps, HTMLAttributes } from "react";
 declare module "react" {
   namespace JSX {
     interface IntrinsicElements {
-      "esp-web-install-button": DetailedHTMLProps<
-        HTMLAttributes<HTMLElement> & { manifest?: string },
-        HTMLElement
-      >;
+      "esp-web-install-button": DetailedHTMLProps<HTMLAttributes<HTMLElement> & { manifest?: string }, HTMLElement>;
     }
   }
 }
