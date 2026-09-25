71588793fb52e6a661adc116f0e9b40c4f1128a2
brainlesslukas <info@brainlesslukas.com>
2026-09-26 01:57:25 +0200

    fix: responsive hero section

--- app/_sections/Hero.tsx
+++ app/_sections/Hero.tsx
@@ -7,16 +7,16 @@ export default function Hero() {
     <section id="hero" className="relative flex min-h-screen items-center justify-center bg-[#F9FAFB] overflow-hidden">
       <Bubbles />
       <div className="z-2">
-        <h1 className="text-8xl font-semibold text-center">CtrlPad</h1>
-        <h2 className="text-3xl text-center font-medium">Make controls fast, launch apps instantly.</h2>
-        <div className="flex justify-center items-center space-x-5 p-5">
-          <Link href="/docs">
-            <Button size="lg" className="w-48 h-12 text-xl bg-[#10B981] hover:bg-[#10B981]/80 rounded-md">
+        <h1 className="text-6xl sm:text-8xl font-semibold text-center">CtrlPad</h1>
+        <h2 className="text-2xl sm:text-3xl text-center font-medium">Make controls fast, launch apps instantly.</h2>
+        <div className="flex flex-col md:flex-row justify-center items-center gap-2 md:gap-5 p-5">
+          <Link href="/docs" className="w-full md:w-auto">
+            <Button size="lg" className="w-full md:w-48 h-12 text-xl bg-[#10B981] hover:bg-[#10B981]/80 rounded-md">
               Get started <ArrowRight size={16} strokeWidth={3} />
             </Button>
           </Link>
-          <Link href="https://github.com/ctrlpad">
-            <Button size="lg" variant="outline" className="w-48 h-12 text-xl rounded-md">
+          <Link href="https://github.com/ctrlpad" className="w-full md:w-auto">
+            <Button size="lg" variant="outline" className="w-full md:w-48 h-12 text-xl rounded-md">
               Github <Star size={16} strokeWidth={3} />
             </Button>
           </Link>
